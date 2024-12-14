6cfc8884b0eb2fbb8e019b2e29e65d590a6b4fcc
Juan Galdo <juan.galdo@ucb.edu.bo>
2024-12-14 11:40:43 -0400

    Creating components files and using route

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-labs',
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
 })
-export class AppComponent {
-  title = 'todoapp';
+export class LabsComponent {
   name = 'Antonio';
   steps = [
     'Install Node.js',
