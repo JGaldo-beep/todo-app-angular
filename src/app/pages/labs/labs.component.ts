ea57d30eae8812af8d7a7844aff9a11942470916
Juan Galdo <juan.galdo@ucb.edu.bo>
2024-12-25 20:56:55 -0400

    Learning Signals

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,7 +8,8 @@ import { CommonModule } from '@angular/common';
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-  name = 'Antonio';
+  name = signal('Antonio');
+  age = 24;
   steps = [
     'Install Node.js',
     'Install Angular CLI',
@@ -16,4 +17,31 @@ export class LabsComponent {
     'Serve the project',
     'Open the project in the browser'
   ];
+  disable = true;
+  img = 'https://th.bing.com/th/id/OIP.07FYICs3oQnX6ziP3-PnnQHaEK?rs=1&pid=ImgDetMain';
+  person = {
+    name: "Wallace",
+    age: 15,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  };
+
+  handleClick() {
+    alert("Hello world!");
+  };
+
+  handleChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  };
+
+  handleKeyup(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+
+  handleKeydown(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
