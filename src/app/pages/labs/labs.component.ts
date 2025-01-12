5b4ad3f61e9e1a631d0ed4e6169ddcb87e877066
Juan Galdo <juan.galdo@ucb.edu.bo>
2025-01-12 00:11:09 -0400

    Learning new sintaxis in Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -7,7 +7,7 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   selector: 'app-labs',
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   name = signal('Antonio');
@@ -17,66 +17,64 @@ export class LabsComponent {
     'Install Angular CLI',
     'Create new Angular project',
     'Serve the project',
-    'Open the project in the browser'
+    'Open the project in the browser',
   ]);
   disable = true;
-  img = 'https://th.bing.com/th/id/OIP.07FYICs3oQnX6ziP3-PnnQHaEK?rs=1&pid=ImgDetMain';
+  img =
+    'https://th.bing.com/th/id/OIP.07FYICs3oQnX6ziP3-PnnQHaEK?rs=1&pid=ImgDetMain';
 
   person = signal<Person>({
-      name: "Wallace",
-      age: 15,
-      avatar: 'https://w3schools.com/howto/img_avatar.png'
+    name: 'Wallace',
+    age: 15,
+    avatar: 'https://w3schools.com/howto/img_avatar.png',
   });
 
   colorCtrl = new FormControl();
   widthCtrl = new FormControl(50, {
-    nonNullable: true
+    nonNullable: true,
   });
   nameCtrl = new FormControl('Wallace', {
     nonNullable: true,
-    validators: [
-      Validators.required,
-      Validators.minLength(3)
-    ]
+    validators: [Validators.required, Validators.minLength(3)],
   });
 
   constructor() {
-    this.colorCtrl.valueChanges.subscribe(value => {
+    this.colorCtrl.valueChanges.subscribe((value) => {
       console.log(value);
     });
-  };
+  }
 
   handleClick() {
-    alert("Hello world!");
-  };
+    alert('Hello world!');
+  }
 
   handleChange(event: Event) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
     this.name.set(newValue);
-  };
+  }
 
   /* Changin Person Attributes */
   changeName(event: Event) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
-    this.person.update(prevState => {
+    this.person.update((prevState) => {
       return {
         ...prevState,
         name: newValue,
-      }
-    })
-  };
+      };
+    });
+  }
 
   changeAge(event: Event) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
-    this.person.update(prevState => {
+    this.person.update((prevState) => {
       return {
         ...prevState,
         age: parseInt(newValue),
-      }
-    })
+      };
+    });
   }
 
   handleKeyup(event: KeyboardEvent) {
